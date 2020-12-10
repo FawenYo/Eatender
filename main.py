7b972766753cda9ea69fa87045b0b584205b83c6
FawenYo <twsky020600@gmail.com>
2020-12-10 20:11:20 +0800

    feat: add API testing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flasgger import Swagger, swag_from
 from flask import Flask, render_template
 
+from API.router import api
 from config import SWAGGER_TEMPLATE, console
 from line.handler import line_app
 
@@ -8,7 +9,10 @@ app = Flask(__name__, static_url_path="/static/")
 swagger = Swagger(app, template=SWAGGER_TEMPLATE)
 # Disable auto sort JSON data when API return values
 app.config["JSON_SORT_KEYS"] = False
+# Return proper json format data
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 app.register_blueprint(line_app)
+app.register_blueprint(api, url_prefix="/api")
 
 
 @app.route("/")
