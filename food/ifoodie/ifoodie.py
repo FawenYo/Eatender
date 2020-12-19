678e0a456cfc068507ab7451eeb7c6583203daea
Morningstar0831 <60170228+moven0831@users.noreply.github.com>
2020-12-19 14:35:09 +0800

    Update ifoodie.py
    只優先取得價格資訊和餐廳url即可

--- food/ifoodie/ifoodie.py
+++ food/ifoodie/ifoodie.py
@@ -5,85 +5,48 @@ from bs4 import BeautifulSoup
 
 
 class Ifoodie:
-    def __init__(self, restaurant_name, latitude, longitude):
+    def __init__(self, restaurant_name, latitude, longitude, request_review=False):
         self.restaurant_name = restaurant_name  # 欲搜尋的餐廳名稱，如"巷子口食堂"
-        self.latitude = latitude
-        self.longitude = longitude
-        self.restaurant_url = self.restaurant_url()
-        self.response = requests.get(self.restaurant_url)
-        self.response.encoding = "utf-8"
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+        self.restaurant_url = str()
         self.info = self.get_info()
-        self.comments = self.get_comments()
+        if request_review:
+            self.comments = self.get_comments()
 
-    def restaurant_url(self):
+    def get_info(self) -> dict:
         # 將經緯度的精度限制在小數點後第5位之內，以便愛食記搜尋
         # 找到範圍1.5公里內的店家就好
         search_url = f"https://ifoodie.tw/explore/list/{self.restaurant_name}?range=1.5&latlng={self.latitude:.5f},{self.longitude:.5f}"
-        response = requests.get(search_url)
+        response = requests.get(search_url, timeout=20)
         response.encoding = "utf-8"
         soup = BeautifulSoup(response.text, "html.parser")
 
         # 找到範圍1.5公里內，第一家符合名稱的店家(搜尋順序是愛食記的搜尋結果)
-        results = soup.find_all("jsx-2133253768 click-tracker")
-        sel = soup.select("a.jsx-2133253768.title-text")
+        url_sel = soup.select("a.jsx-2133253768.title-text")
         fragment_url = ""
-        for each in sel:
-            if self.restaurant_name in each.text:
-                fragment_url = re.findall(r"href=\"(.*)\" target", str(each))[0]
+        target_index = int()
+        for i in range(len(url_sel)):
+            if self.restaurant_name in url_sel[i].text:
+                fragment_url = re.findall(r"href=\"(.*)\" target", str(url_sel[i]))[0]
+                target_index = i
                 break
-        url = "https://ifoodie.tw" + fragment_url
-        return url
-
-    def get_info(self) -> dict:
-        soup = BeautifulSoup(self.response.text, "html.parser")
-        sel = soup.select("div.jsx-558709029.info")
+        self.restaurant_url = "https://ifoodie.tw" + fragment_url
+        
+        price_sel = soup.select("div.jsx-2133253768.avg-price")
+        avg_price = re.findall(r"[0-9]+", str(price_sel[target_index]))[1]
 
         info = {
-            "營業": "尚無營業時間資訊",  # temp
+            "營業時間": "尚無營業時間資訊",
             "店家地址": "尚無店家地址資訊",
             "聯絡電話": "尚無聯絡電話資訊",
-            "均消價位": "尚無均消價位資訊",
+            "均消價位": str(avg_price),
         }
-        raw_info = sel[0].text
-        keys = list(info.keys())
-
-        for i in range(len(keys)):
-            if keys[i] in raw_info:
-                start = raw_info.find(keys[i]) + len(keys[i]) + 1
-                next_keyword_index = -1
-                for j in range(len(keys)):
-                    if i < j and keys[j] in raw_info[start:]:
-                        next_keyword_index = raw_info.find(keys[j])
-                        break
-
-                if next_keyword_index != -1:
-                    info[keys[i]] = raw_info[start:next_keyword_index].strip("")
-                else:
-                    if keys[i] == "聯絡電話":
-                        phone_candidate = []
-                        raw_string = raw_info[start:]
-                        for m in range(len(raw_string)):
-                            for n in range(m, len(raw_string)):
-                                if raw_string[m:n].isdigit():
-                                    phone_candidate.append(raw_string[m:n].strip(" "))
-                        info[keys[i]] = max(phone_candidate, key=len)
-
-                    elif keys[i] == "均消價位":
-                        rating_candidate = []
-                        raw_string = raw_info[start:]
-                        for m in range(len(raw_string)):
-                            for n in range(m, len(raw_string)):
-                                if raw_string[m:n].isdigit():
-                                    rating_candidate.append(raw_string[m:n].strip(" "))
-                        info[keys[i]] = max(rating_candidate, key=len)
-
-        # 取正確的時間到營業時間中
-        info["營業時間"] = info.pop("營業")
-
         return info
 
     def get_comments(self) -> list:
-        soup = BeautifulSoup(self.response.text, "html.parser")
+        response = requests.get(self.restaurant_url, timeout=20)
+        soup = BeautifulSoup(response.text, "html.parser")
         content_sel = soup.select("div.jsx-2738468548.message")
 
         comments = []
