843a04176c480553e8aa5bb35f2b7569454cd416
FawenYo <twsky020600@gmail.com>
2020-12-30 12:45:39 +0800

    Feat: Poetry for project environment management

--- project/gunicorn.py
+++ project/gunicorn.py
@@ -3,6 +3,13 @@ import os
 # Gunicorn settings
 port = int(os.getenv("PORT", 8001))
 bind = f"0.0.0.0:{port}"
-loglevel = "debug"
+
+profile = os.getenv("profile", "production")
+if profile == "production":
+    loglevel = "info"
+# development
+else:
+    loglevel = "debug"
+
 workers = 4
 threads = 4
