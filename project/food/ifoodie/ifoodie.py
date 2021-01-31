282bb9b82302b07dea316e7136949088d2c1cb93
Morningstar0831 <60170228+moven0831@users.noreply.github.com>
2021-01-31 16:19:07 +0800

    Update ifoodie.py
    fixed ifoodie url missing error

--- project/food/ifoodie/ifoodie.py
+++ project/food/ifoodie/ifoodie.py
@@ -18,21 +18,26 @@ class Ifoodie:
     def get_info(self) -> dict:
         # 將經緯度的精度限制在小數點後第5位之內，以便愛食記搜尋
         # 找到範圍1.5公里內的店家就好
-        search_url = f"https://ifoodie.tw/explore/list/{self.restaurant_name}?range=1.5&latlng={self.latitude:.5f},{self.longitude:.5f}"
+        legal_name = self.restaurant_name.replace(" ", "%20")
+        search_url = f"https://ifoodie.tw/explore/list/{legal_name}?range=1.5&latlng={self.latitude:.5f},{self.longitude:.5f}"
         response = requests.get(search_url, timeout=20)
         response.encoding = "utf-8"
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # 找到範圍1.5公里內，第一家符合名稱的店家(搜尋順序是愛食記的搜尋結果)
-        url_sel = soup.select("a.jsx-2133253768.title-text")
-        fragment_url = ""
-        target_index = int()
-        for i in range(len(url_sel)):
-            if self.restaurant_name in url_sel[i].text:
-                fragment_url = re.findall(r"href=\"(.*)\" target", str(url_sel[i]))[0]
-                target_index = i
-                break
-        self.restaurant_url = "https://ifoodie.tw" + fragment_url
+        try:
+            url_sel = soup.select("a.jsx-2133253768.title-text")
+            if not url_sel:
+                legal_name = legal_name.split("-")[0]
+                search_url = f"https://ifoodie.tw/explore/list/{legal_name}?range=1.5&latlng={self.latitude:.5f},{self.longitude:.5f}"
+                response = requests.get(search_url, timeout=20)
+                response.encoding = "utf-8"
+                soup = BeautifulSoup(response.text, "html.parser")
+                url_sel = soup.select("a.jsx-2133253768.title-text")
+            fragment_url = re.findall(r"href=\"(.*)\" target", str(url_sel[0]))[0]
+            self.restaurant_url = "https://ifoodie.tw" + fragment_url
+        except:
+            self.restaurant_url = "https://ifoodie.tw"
+            pass
 
         info = {
             "營業時間": "尚無營業時間資訊",
@@ -43,18 +48,18 @@ class Ifoodie:
 
         try:
             price_sel = soup.select("div.jsx-2133253768.avg-price")
-            info["均消價位"] = re.findall(r"[0-9]+", str(price_sel[target_index]))[1]
+            info["均消價位"] = re.findall(r"[0-9]+", str(price_sel[0]))[1]
 
             info_sel = soup.select("div.jsx-2133253768.info")
             operating_info = re.findall(
-                r"</span>(.*)</div>", str(info_sel[target_index])
+                r"</span>(.*)</div>", str(info_sel[0])
             )[0]
             temp = operating_info.split("營業:")
             info["營業時間"] = temp[1].strip(" ")
 
             address_sel = soup.select("div.jsx-2133253768.address-row")
             info["店家地址"] = re.findall(
-                r"address-row\">(.*)</div>", str(address_sel[target_index])
+                r"address-row\">(.*)</div>", str(address_sel[0])
             )[0]
         except:
             pass
