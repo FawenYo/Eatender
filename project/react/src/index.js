bfb9c357a3a0b422819486503af7f0708645ac99
FawenYo <twsky020600@gmail.com>
2021-02-07 22:18:21 +0800

    refractor: code format

--- project/react/src/index.js
+++ project/react/src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { render } from "react-dom";
-import Form from "./components/Form";
-import Banner from "./components/Banner";
+import React from "react"
+import { render } from "react-dom"
+import Form from "./components/Form"
+import Banner from "./components/Banner"
 
 const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
-};
+    fontFamily: "sans-serif",
+    textAlign: "center",
+}
 
 const DateTimes = () => (
-  <div style={styles}>
-    <DateRange />
-  </div>
-);
+    <div style={styles}>
+        <DateRange />
+    </div>
+)
 
-render(<Banner />, document.querySelector("#banner"));
-render(<Form textAlign="center"/>, document.querySelector("#form"));
+// render(<Banner />, document.querySelector("#banner"));
+render(<Form textAlign="center" />, document.querySelector("#form"))
