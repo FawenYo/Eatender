8ae6074a5e678d070737edb0573f109ada81cfb7
shanpig <shanpigliao@gmail.com>
2021-02-11 10:24:43 +0800

    remove confirm button from schedular

--- react/src/components/Schedular.js
+++ react/src/components/Schedular.js
@@ -1,8 +1,9 @@
 import ScheduleSelector from 'react-schedule-selector';
+import styled from 'styled-components';
 import React, {useState} from 'react';
 
 // 在要使用 schedular 的 js 檔中直接插入元件，並加入
-// "總天數" "開始時間" "結束時間" 等參數，以及一個取得 schedule 的 callback。如下： 
+// "總天數" "開始時間" "結束時間" 等參數，以及一個取得 schedule 的 callback。舉例如下： 
 // 
 //  <Schedular
 //    _startDate = {new Date()}
@@ -21,33 +22,33 @@ import React, {useState} from 'react';
 //    console.log(schedule); 
 //  }
 //
-function Schedular({_startDate, _numDays, _minTime, _maxTime, passScheduleOut}) {
+const Wrapper = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  width: 80%`;
+
+const ScheduleWrapper = styled.div`
+  padding: 20px;
+  border-radius: 20px;
+  box-shadow: 3px 5px 18px 0px rgba(191,191,191,0.76)`;
+
+function App({_numDays, _minTime, _maxTime, passScheduleOut}) {
   const [schedule, setSchedule] = useState([]);
 
-  function handleChange(newSchedule) {setSchedule(newSchedule)};
-
+  function handleChange(newSchedule) {
+    setSchedule(newSchedule);
+    passScheduleOut(newSchedule);
+  }
   return (
-    <div style={{textAlign: "center", margin: "0 auto", width:"80%"}}>
+    <Wrapper>
       <header>
         <h1>React Schedule Selector</h1>
         <p>Tap to select one time or drag to select multiple times at once.</p>
-        <button 
-          style={{
-            minWidth: "50px", maxWidth: "200px", width: "10%",
-            minHeight: "30px", maxWidth: "120px", width: "6%"
-          }} 
-          onClick={()=>passScheduleOut(schedule)}>確認</button>
       </header>
       <br/>
-      <div
-        style={{
-          padding: "20px",
-          borderRadius: "20px",
-          boxShadow: "3px 5px 18px 0px rgba(191,191,191,0.76)"
-        }}>
+      <ScheduleWrapper>
         <ScheduleSelector
           selection={schedule}
-          startDate={_startDate}
           numDays={_numDays}
           minTime={_minTime}
           maxTime={_maxTime}
@@ -55,9 +56,9 @@ function Schedular({_startDate, _numDays, _minTime, _maxTime, passScheduleOut}) 
           onChange={handleChange}
           timeFormat={"hh:mma"}
         />
-      </div>
-    </div>
+      </ScheduleWrapper>
+    </Wrapper>
   )
 }
 
-export default Schedular;
+export default App;
